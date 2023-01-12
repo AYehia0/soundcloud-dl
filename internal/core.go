b948403f916145aca27f5799783d7a7270ca579b
AYehia0 <ahmedyehia611@gmail.com>
2023-01-12 20:24:07 +0200

    Cleaned the core base

--- internal/core.go
+++ internal/core.go
@@ -14,24 +14,7 @@ import (
 
 var defaultQuality string = "low"
 
-func initValidations(url string) bool {
-
-	fmt.Printf("%s Validating the URL : %s\n", theme.Yellow("[+]"), theme.Magenta(url))
-
-	// check if the url is valid
-	if !soundcloud.IsValidUrl(url) {
-		fmt.Printf("%s The Url : %s isn't a valid soundcloud URL\n", theme.Red("[+]"), theme.Magenta(url))
-		return false
-	}
-
-	fmt.Printf("%s URL is Valid! \n", theme.Green("[+]"))
-	fmt.Printf("%s Checking The Track on Soundcloud. \n", theme.Red("[+]"))
-
-	return true
-
-}
-
-func Sc(args []string) {
+func Sc(args []string, bestQuality bool) {
 
 	url := args[0]
 	downloadPath := args[len(args)-1]
@@ -46,7 +29,7 @@ func Sc(args []string) {
 		log.Fatalf("An Error : %s happended while requesting : %s", err, url)
 	}
 	if statusCode != http.StatusOK {
-		fmt.Println("URL doesn't exist : status not 200.")
+		fmt.Println("URL doesn't exist : status is : ", statusCode)
 		return
 	}
 
@@ -64,31 +47,13 @@ func Sc(args []string) {
 	list := soundcloud.GetFormattedDL(soundData.Transcodings, clientId)
 
 	// show available download options
-	printQualities(list)
+	qualities := getQualities(list)
+	if !bestQuality {
+		defaultQuality = chooseQuality(qualities)
+	} else {
+		defaultQuality = getHighestQuality(qualities)
+	}
+
 	soundcloud.Download(chooseTrackDownload(list, defaultQuality), downloadPath)
 	fmt.Printf("\n%s Track saved to : %s\n", theme.Green("[-]"), theme.Magenta(downloadPath))
-}
-
-// TEMP: Just for now, return the quality
-// the default quality is just mp3, highest is ogg
-// if the quality doesn't exist return the first one
-func chooseTrackDownload(tracks []soundcloud.DownloadTrack, target string) soundcloud.DownloadTrack {
-	for _, track := range tracks {
-		if track.Quality == target {
-			return track
-		}
-	}
-	return tracks[0]
-}
-
-func printQualities(tracks []soundcloud.DownloadTrack) {
-	fmt.Printf("%s Available Qualities :\n", theme.Green("[+]"))
-	for i, track := range tracks {
-		// check the default one
-		if track.Quality == defaultQuality {
-			fmt.Printf("\t[%s]- %s %s\n", theme.Magenta(i+1), theme.Green(track.Quality), theme.Magenta("(selected)"))
-		} else {
-			fmt.Printf("\t[%s]- %s\n", theme.Magenta(i+1), theme.Green(track.Quality))
-		}
-	}
 }
---
+++ internal/utils.go
@@ -0,0 +1,81 @@
+package internal
+
+import (
+	"fmt"
+
+	"github.com/AYehia0/soundcloud-dl/pkg/soundcloud"
+	"github.com/AYehia0/soundcloud-dl/pkg/theme"
+	"github.com/manifoldco/promptui"
+)
+
+func initValidations(url string) bool {
+
+	fmt.Printf("%s Validating the URL : %s\n", theme.Yellow("[+]"), theme.Magenta(url))
+
+	// check if the url is valid
+	if !soundcloud.IsValidUrl(url) {
+		fmt.Printf("%s The Url : %s isn't a valid soundcloud URL\n", theme.Red("[+]"), theme.Magenta(url))
+		return false
+	}
+
+	fmt.Printf("%s URL is Valid! \n", theme.Green("[+]"))
+	fmt.Printf("%s Checking The Track on Soundcloud. \n", theme.Red("[+]"))
+
+	return true
+
+}
+
+// TEMP: Just for now, return the quality
+// the default quality is just mp3, highest is ogg
+// if the quality doesn't exist return the first one
+func chooseTrackDownload(tracks []soundcloud.DownloadTrack, target string) soundcloud.DownloadTrack {
+	for _, track := range tracks {
+		if track.Quality == target {
+			return track
+		}
+	}
+	return tracks[0]
+}
+
+func getQualities(tracks []soundcloud.DownloadTrack) []string {
+	qualities := make([]string, 0)
+	for _, track := range tracks {
+		// check the default one
+		qualities = append(qualities, track.Quality)
+	}
+	return qualities
+}
+
+// prompt select quality, only if --best not passed
+func chooseQuality(qualities []string) string {
+	fmt.Printf("%s Available Qualities :\n", theme.Green("[+]"))
+	prompt := promptui.Select{
+		Label: "Choose a quality:",
+		Items: qualities,
+	}
+	_, q, err := prompt.Run()
+	if err != nil {
+		fmt.Printf("Prompt Failed : %s, continuing with : %s quality instead!", err, qualities[0])
+		return qualities[0]
+	}
+	return q
+}
+
+func getHighestQuality(qualities []string) string {
+	allQualities := []string{"high", "medium", "low"}
+	var in = func(a string, list []string) bool {
+		for _, b := range list {
+			if b == a {
+				return true
+			}
+		}
+		return false
+	}
+
+	for _, q := range allQualities {
+		if in(q, qualities) {
+			return q
+		}
+	}
+	return ""
+}
