82e9c39af59e2e210b0e690c0ffc6a2da96c4203
AYehia0 <ahmedyehia611@gmail.com>
2023-01-13 09:05:12 +0200

    Added artwork_url, used to be added as a file tag

--- pkg/soundcloud/types.go
+++ pkg/soundcloud/types.go
@@ -14,6 +14,8 @@ type SoundData struct {
 	Kind         string `json:"kind"`
 	PermalinkUrl string `json:"permalink_url"`
 	UserId       int64  `json:"user_id"`
+	ArtworkUrl   string `json:"artwork_url"`
+	Genre        string `json:"genre"`
 	Transcodings []Transcode
 	TrackAuth    string `json:"track_authorization"`
 	LikesCount   int    `json:"likes_count"`
