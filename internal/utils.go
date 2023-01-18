8e6d23176a2b03e8a4010247fbfa7d0ccc9ee263
AYehia0 <ahmedyehia611@gmail.com>
2023-01-18 09:25:34 +0200

    Throwing some ugly ass go concurrent to speed up the playlist parsing.
    - Added Yes?No prompt for downloading only the playlist.
    - Added speeeeeed Yeahh boi.

--- internal/utils.go
+++ internal/utils.go
@@ -3,6 +3,7 @@ package internal
 import (
 	"fmt"
 	"os"
+	"sync"
 
 	"github.com/AYehia0/soundcloud-dl/pkg/soundcloud"
 	"github.com/AYehia0/soundcloud-dl/pkg/theme"
@@ -38,6 +39,8 @@ func chooseTrackDownload(tracks []soundcloud.DownloadTrack, target string) sound
 	return tracks[0]
 }
 
+// get all the available qualities inside the track
+// used to choose a track to download based on the target quality
 func getQualities(tracks []soundcloud.DownloadTrack) []string {
 	qualities := make([]string, 0)
 	for _, track := range tracks {
@@ -50,6 +53,10 @@ func getQualities(tracks []soundcloud.DownloadTrack) []string {
 // prompt select quality, only if --best not passed
 func chooseQuality(qualities []string) string {
 	fmt.Printf("%s Available Qualities :\n", theme.Green("[+]"))
+	if len(qualities) < 1 {
+		fmt.Printf("%s No qualities available to download!:\n", theme.Red("[-]"))
+		os.Exit(0)
+	}
 	prompt := promptui.Select{
 		Label: "Choose a quality:",
 		Items: qualities,
@@ -116,4 +123,59 @@ func getUserSearch() string {
 
 	return result
 
+}
+
+func getPlaylistDownloadTracks(soundData *soundcloud.SoundData, clientId string) [][]soundcloud.DownloadTrack {
+
+	var wg sync.WaitGroup
+	listDownloadTracks := make([][]soundcloud.DownloadTrack, 0)
+
+	playlistTracks := soundcloud.GetPlaylistTracks(soundData, clientId)
+
+	fmt.Printf("%s Playlist contains : %s track\n", theme.Green("[+]"), theme.Magenta(len(playlistTracks)))
+	if !promptYesNo() {
+		fmt.Printf("%s Exiting...\n", theme.Red("[-]"))
+		os.Exit(0)
+	}
+	for i, t := range playlistTracks {
+		wg.Add(1)
+
+		go func(t soundcloud.SoundData) {
+			defer wg.Done()
+			dlTrack := soundcloud.GetFormattedDL(&t, clientId)
+			listDownloadTracks = append(listDownloadTracks, dlTrack)
+		}(t)
+		fmt.Printf("%s  %v - %s \n", theme.Green("[+]"), theme.Red(i+1), theme.Magenta(t.Title))
+	}
+	wg.Wait()
+	return listDownloadTracks
+}
+
+// get a final track to be downloaded
+// if bestQuality is false it will prompt the user to choose a quality
+func getTrack(downloadTracks []soundcloud.DownloadTrack, bestQuality bool) soundcloud.DownloadTrack {
+
+	// show available download options
+	qualities := getQualities(downloadTracks)
+	if !bestQuality {
+		defaultQuality = chooseQuality(qualities)
+	} else {
+		defaultQuality = getHighestQuality(qualities)
+	}
+
+	return chooseTrackDownload(downloadTracks, defaultQuality)
+
+}
+
+func promptYesNo() bool {
+	prompt := promptui.Select{
+		Label: "Download [Yes/No]",
+		Items: []string{"Yes", "No"},
+	}
+	_, result, err := prompt.Run()
+	if err != nil {
+		fmt.Printf("%s Download aborted!\n", theme.Red("[-]"))
+		os.Exit(0)
+	}
+	return result == "Yes"
 }
